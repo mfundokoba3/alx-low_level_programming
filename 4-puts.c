2ac6a08a1cd7e30f5a3e724cdd4c101b9e809aae
mfundokoba3 <mfundokc@gmail.com>
2022-07-03 15:33:34 -0700

    puts

---
+++ 4-puts.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+
+{
+
+/*print string set as array
+ *string is "programming is like building a multilingual puzzle"
+ */
+
+chart str1[] = "programming is like building a multilingual puzzle"
+
+/* writing to stdout
+*/
+puts(str1);
+
+return 0;
+
+}
+
+
