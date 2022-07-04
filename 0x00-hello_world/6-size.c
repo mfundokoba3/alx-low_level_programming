395f70bdbe4d9abd2ade443c3066b57fb876ef59
mfundokoba3 <mfundokc@gmail.com>
2022-07-04 15:23:54 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* This program prints size of data types
+*/
+int main(void)
+{
+char a;
+int c;
+int d;
+float e;
+
+printf("size of a char: %lu.\n", sizeof(a));
+printf("size of an int: %lu.\n", sizeof(b));
+printf("size of a long int: %lu.\n", sizeof(c))
+printf("size of a long long int: %lu.\n", sizeof(d));
+
+return(0);
+}
