29c9554240073683da67a26ea2395207fcfb7f5a
mfundokoba3 <mfundokc@gmail.com>
2022-07-03 17:33:47 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - prints string
-* Return: 0 
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
