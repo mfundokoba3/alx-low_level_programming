c924974348a0583ed4eba106602c5d8389b8e98c
mfundokoba3 <mfundokc@gmail.com>
2022-07-06 13:59:25 -0700

    lastdigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * @brief The function is to print output from generated variables
+ * brief The function is to print output from generated variables
  * 
- * @return int: 0 
+ * return int: 0 
  */
 int main(void)
 {
@@ -11,17 +11,20 @@ int main(void)
 /**
  * @brief print int from generated number 
  */
-if (n > 5)
-{
-    printf("Last digit %d and is greater than 5\n", n);
-}
-else if (n == 0)
-{
-    printf("Last digit %d and is 0"\n, n)
-}
-else (n < 6)
-{
-    printf("Last digit is %d and is less than 6 and not 0\n", n)
-}
+	if (n > 5)
+	{
+    		printf("Last digit %d and is greater than 5", n);
+	}
+	else if (n == 0)
+	{
+    		printf("Last digit %d and is 0", n);
+	}
+	else (n < 6 && n != 0)
+	{
+    		printf("Last digit is %d and is less than 6 and not 0", n)
+	}
+	
+	printf("\n");
+
 	return (0);
 }
