0d21a266323b692fd818ba8914a709087f1d72da
mfundokoba3 <mfundokc@gmail.com>
2022-07-06 13:44:24 -0700

    lastdigit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * @brief The function is to print output from generated variables
+ * 
+ * @return int: 0 
+ */
+int main(void)
+{
+	int n;
+/**
+ * @brief print int from generated number 
+ */
+if (n > 5)
+{
+    printf("Last digit %d and is greater than 5\n", n);
+}
+else if (n == 0)
+{
+    printf("Last digit %d and is 0"\n, n)
+}
+else (n < 6)
+{
+    printf("Last digit is %d and is less than 6 and not 0\n", n)
+}
+	return (0);
+}
